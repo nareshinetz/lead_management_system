4bcb4f228ad7b981863afcde90c84dc5cb2bcefa
imkarthiksk <imkarthik.sk@gmail.com>
2026-05-28 21:06:58 +0530

    User schema created

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  role: {
+    type: String,
+    default: "counsellor",
+  },
+ status: {
+  type: String,
+  default: "active",
+}
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
